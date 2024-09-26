7b41a062ca49ffa182a1d8cd972d0581ad7d771b
Ian M. Nava Garduno <ianmng1@gmail.com>
2024-09-25 20:47:13 -0600

    Ability to edit Profile info done

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,15 +1,32 @@
 import React, { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 import FullContext from "../FullContext";
 
 function Profile() {
   const { state, dispatch } = useContext(FullContext);
-  const [edit, setEdition] = useState(false);
+  const [edit, setEdition] = useState(true);
   const gender = state.user.gender;
   const cardType = state.user.card;
+  const navigate = useNavigate();
 
   function changeCallback(e, inputType) {
+    if (!edit) {
+      dispatch({ type: inputType, value: e.target.value });
+    }
+  }
+
+  async function editSaveInfo() {
     if (edit) {
+      setEdition(false);
+      document
+        .querySelectorAll(".p-edit-state")
+        .forEach((el) => el.classList.remove("p-edit-state"));
+    } else {
+      setEdition(true);
+      // Request goes here
+      navigate(`/`);
+      alert("Information updated");
     }
   }
 
@@ -20,13 +37,8 @@ function Profile() {
       </section>
 
       <main id="profile-edit-c">
-        <button
-          id="profile-edit-btn"
-          onClick={() => {
-            setEdition(true);
-          }}
-        >
-          Edit info
+        <button id="profile-edit-btn" onClick={editSaveInfo}>
+          {edit ? "Edit info" : "Save"}
         </button>
 
         <div className="c-inp">
@@ -34,19 +46,21 @@ function Profile() {
           <input
             type="text"
             id="username"
-            className="p-e-inp"
+            className="p-e-inp p-edit-state"
             value={state.user.username}
+            readOnly={edit}
             onChange={(e) => changeCallback(e, "username")}
           />
         </div>
 
         <div className="c-inp">
-          <label htmlFor="fullname">Fullname:</label>{" "}
+          <label htmlFor="fullname">Full name:</label>{" "}
           <input
             type="text"
             id="fullname"
-            className="p-e-inp"
+            className="p-e-inp p-edit-state"
             value={state.user.fullName}
+            readOnly={edit}
             onChange={(e) => changeCallback(e, "fullName")}
           />
         </div>
@@ -56,8 +70,9 @@ function Profile() {
           <input
             type="text"
             id="email"
-            className="p-e-inp"
+            className="p-e-inp p-edit-state"
             value={state.user.email}
+            readOnly={edit}
             onChange={(e) => changeCallback(e, "email")}
           />
         </div>
@@ -67,8 +82,9 @@ function Profile() {
           <input
             type="password"
             id="password"
-            className="p-e-inp"
+            className="p-e-inp p-edit-state"
             value={state.user.password}
+            readOnly={edit}
             onChange={(e) => changeCallback(e, "password")}
           />
         </div>
@@ -77,22 +93,25 @@ function Profile() {
           <label htmlFor="gender">Gender:</label>
 
           <div className="profile-gen-cont">
-            <label>
+            <label className="p-edit-state">
               <input
                 checked={gender === "male"}
                 type="radio"
+                id="gender"
                 name="gender"
                 value="male"
+                readOnly={edit}
                 onChange={(e) => changeCallback(e, "gender")}
               />{" "}
               Male
             </label>{" "}
-            <label>
+            <label className="p-edit-state">
               <input
                 checked={gender === "female"}
                 type="radio"
                 name="gender"
                 value="female"
+                readOnly={edit}
                 onChange={(e) => changeCallback(e, "gender")}
               />{" "}
               Female
@@ -103,10 +122,11 @@ function Profile() {
         <div className="c-inp">
           <label htmlFor="cardType">Debit-card:</label>{" "}
           <select
-            className="p-e-inp"
+            className="p-e-inp p-edit-state"
             name="cardType"
             id="cardType"
             onChange={(e) => changeCallback(e, "card")}
+            disabled={edit}
           >
             <option value="student" selected={cardType === "student"}>
               Student account
@@ -123,8 +143,11 @@ function Profile() {
         <div className="c-inp address">
           <label htmlFor="address">Address:</label>{" "}
           <textarea
+            className="p-edit-state"
             name="address"
             id="address"
+            value={state.user.address}
+            readOnly={edit}
             onChange={(e) => changeCallback(e, "address")}
           ></textarea>
         </div>
